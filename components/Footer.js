11c2c76e72e1495c4af5b5eb7996f5d9e58ae92b
DSM5 <fatec-dsm5@academico.intra>
2024-06-25 22:09:29 -0300

    Instalando imagem e fazendo componente de 404.js

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,11 @@
 import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={styles.footer}>
-        <p><span>PokeNext</span> &copy; 2021</p>
+        <p><span>PokeNext</span> &copy; {currentYear}</p>
     </footer>
   )
 }
--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,17 @@
+import Image from 'next/image';
+import styles from '../styles/Notfound.module.css'
+
 export default function NotFound() {
   return (
-    <>
+    <div className={styles.notfound}>
       <h1>Página não encontrada</h1>
       <p>Ops, parece que esse pokemon não existe!</p>
-    </>
+      <Image
+        src="/images/notfound-image.png"
+        width={150}
+        height={150}
+        unoptimezed
+      />
+    </div>
   );
 }
